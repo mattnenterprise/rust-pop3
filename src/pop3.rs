af74c70be9e423fe766ccf34ba795d6b384e711a
Matt McCoy <mattnenterprise@yahoo.com>
2014-11-21 16:49:38 -0500

    Updating client for nightly Rust build

--- src/pop3.rs
+++ src/pop3.rs
@@ -1,6 +1,6 @@
 #![crate_name = "pop3"]
 #![crate_type = "lib"]
-#![feature(struct_variant, phase)]
+#![feature(phase)]
 
 extern crate openssl;
 extern crate regex;
@@ -23,7 +23,7 @@ pub struct POP3Stream {
 	pub is_authenticated: bool
 }
 
-pub enum POP3Command {
+enum POP3Command {
 	Greet,
 	User,
 	Pass,
@@ -43,10 +43,10 @@ impl POP3Stream {
 		let connect_string = format!("{}:{}", host, port);
 		let tcp_stream = try!(TcpStream::connect(connect_string.as_slice()));
 		let mut socket = match ssl_context {
-			Some(context) => POP3Stream {stream: Ssl(SslStream::new(&context, tcp_stream).unwrap()), host: host, port: port, is_authenticated: false},
-			None => POP3Stream {stream: Basic(tcp_stream), host: host, port: port, is_authenticated: false},
+			Some(context) => POP3Stream {stream: POP3StreamTypes::Ssl(SslStream::new(&context, tcp_stream).unwrap()), host: host, port: port, is_authenticated: false},
+			None => POP3Stream {stream: POP3StreamTypes::Basic(tcp_stream), host: host, port: port, is_authenticated: false},
 		};
-		match socket.read_response(Greet) {
+		match socket.read_response(POP3Command::Greet) {
 			Ok(_) => {
 				//Do nothing
 			},
@@ -57,35 +57,35 @@ impl POP3Stream {
 
 	fn write_str(&mut self, s: &str) -> IoResult<()> {
 		match self.stream {
-			Ssl(ref mut stream) => stream.write_str(s),
-			Basic(ref mut stream) => stream.write_str(s),
+			POP3StreamTypes::Ssl(ref mut stream) => stream.write_str(s),
+			POP3StreamTypes::Basic(ref mut stream) => stream.write_str(s),
 		}
 	}
 
 	fn read(&mut self, buf: &mut [u8]) -> IoResult<uint> {
 		match self.stream {
-			Ssl(ref mut stream) => stream.read(buf),
-			Basic(ref mut stream) => stream.read(buf),
+			POP3StreamTypes::Ssl(ref mut stream) => stream.read(buf),
+			POP3StreamTypes::Basic(ref mut stream) => stream.read(buf),
 		}
 	}
 
 	pub fn login(&mut self, username: &str, password: &str) -> POP3Result {
-		let user_command = format!("USER {:s}\r\n", username);
-		let pass_command = format!("PASS {:s}\r\n", password);
+		let user_command = format!("USER {}\r\n", username);
+		let pass_command = format!("PASS {}\r\n", password);
 		//Send user command
 		match self.write_str(user_command.as_slice()) {
 			Ok(_) => {},
 			Err(_) => panic!("Error writing"),
 		}
-		match self.read_response(User) {
+		match self.read_response(POP3Command::User) {
 			Ok(_) => {
 				match self.write_str(pass_command.as_slice()) {
 					Ok(_) => self.is_authenticated = true,
 					Err(_) => panic!("Error writing"),
 				}
-				match self.read_response(Pass) {
+				match self.read_response(POP3Command::Pass) {
 					Ok(_) => {
-						POP3Ok
+						POP3Result::POP3Ok
 					},
 					Err(_) => panic!("Failure to use PASS")
 				}
@@ -104,14 +104,14 @@ impl POP3Stream {
 			Ok(_) => {},
 			Err(_) => panic!("Error writing"),
 		}
-		match self.read_response(Stat) {
+		match self.read_response(POP3Command::Stat) {
 			Ok(res) => {
 				match res.result {
 					Some(s) => s,
-					None => POP3Err
+					None => POP3Result::POP3Err
 				}
 			},
-			Err(_) => POP3Err
+			Err(_) => POP3Result::POP3Err
 		}
 	}
 
@@ -125,8 +125,8 @@ impl POP3Stream {
 							None => format!("LIST\r\n"),
 						};
 		let command_type = match message_number {
-							Some(_) => ListOne,
-							None => ListAll,
+							Some(_) => POP3Command::ListOne,
+							None => POP3Command::ListAll,
 						};
 
 		match self.write_str(list_command.as_slice()) {
@@ -138,10 +138,10 @@ impl POP3Stream {
 			Ok(res) => {
 				match res.result {
 					Some(s) => s,
-					None => POP3Err
+					None => POP3Result::POP3Err
 				}
 			},
-			Err(_) => POP3Err
+			Err(_) => POP3Result::POP3Err
 		}
 	}
 
@@ -157,14 +157,14 @@ impl POP3Stream {
 			Err(_) => panic!("Error writing"),
 		}
 
-		match self.read_response(Retr) {
+		match self.read_response(POP3Command::Retr) {
 			Ok(res) => {
 				match res.result {
 					Some(s) => s,
-					None => POP3Err
+					None => POP3Result::POP3Err
 				}
 			},
-			Err(_) => POP3Err
+			Err(_) => POP3Result::POP3Err
 		}
 	}
 
@@ -180,14 +180,14 @@ impl POP3Stream {
 			Err(_) => panic!("Error writing"),
 		}
 
-		match self.read_response(Dele) {
+		match self.read_response(POP3Command::Dele) {
 			Ok(res) => {
 				match res.result {
 					Some(s) => s,
-					None => POP3Err
+					None => POP3Result::POP3Err
 				}
 			},
-			Err(_) => POP3Err
+			Err(_) => POP3Result::POP3Err
 		}
 	}
 
@@ -199,14 +199,14 @@ impl POP3Stream {
 			Err(_) => panic!("Error writing"),
 		}
 
-		match self.read_response(Rset) {
+		match self.read_response(POP3Command::Rset) {
 			Ok(res) => {
 				match res.result {
 					Some(s) => s,
-					None => POP3Err
+					None => POP3Result::POP3Err
 				}
 			},
-			Err(_) => POP3Err
+			Err(_) => POP3Result::POP3Err
 		}
 	}
 
@@ -218,14 +218,14 @@ impl POP3Stream {
 			Err(_) => panic!("Error writing"),
 		}
 
-		match self.read_response(Quit) {
+		match self.read_response(POP3Command::Quit) {
 			Ok(res) => {
 				match res.result {
 					Some(s) => s,
-					None => POP3Err
+					None => POP3Result::POP3Err
 				}
 			},
-			Err(_) => POP3Err
+			Err(_) => POP3Result::POP3Err
 		}
 	}
 
@@ -237,11 +237,11 @@ impl POP3Stream {
 			Err(_) => panic!("Error writing"),
 		}
 
-		match self.read_response(Noop) {
+		match self.read_response(POP3Command::Noop) {
 			Ok(res) => {
 				match res.result {
 					Some(s) => s,
-					None => POP3Err
+					None => POP3Result::POP3Err
 				}
 			},
 			Err(_) => panic!("Error noop")
@@ -258,7 +258,7 @@ impl POP3Stream {
 
 		while !response.complete {
 			while line_buffer.len() < 2 || (line_buffer[line_buffer.len()-1] != lf && line_buffer[line_buffer.len()-2] != cr) {
-				let mut byte_buffer = [0];
+				let byte_buffer: &mut [u8] = &mut [0];
 				match self.read(byte_buffer) {
 					Ok(_) => {},
 					Err(_) => println!("Error Reading!"),
@@ -325,40 +325,40 @@ impl POP3Response {
 			if ok_regex.is_match(l.as_slice()) {
 				self.lines.push(l);
 				match command {
-					Greet|User|Pass|Quit|Dele|Rset => {
-						self.result = Some(POP3Ok);
+					POP3Command::Greet|POP3Command::User|POP3Command::Pass|POP3Command::Quit|POP3Command::Dele|POP3Command::Rset => {
+						self.result = Some(POP3Result::POP3Ok);
 						self.complete = true;
 					},
-					Stat => {
+					POP3Command::Stat => {
 						self.complete = true;
 						self.parse_stat()
 					},
-					ListAll => {
+					POP3Command::ListAll => {
 
 					},
-					ListOne => {
+					POP3Command::ListOne => {
 						self.complete = true;
 						self.parse_list_one();
 					},
-					Retr => {
+					POP3Command::Retr => {
 
 					},
 					_ => self.complete = true,
 				}
 			} else if err_regex.is_match(l.as_slice()) {
 				self.lines.push(l);
-				self.result = Some(POP3Err);
+				self.result = Some(POP3Result::POP3Err);
 				self.complete = true;
 			}
 		} else {
 			if ending_regex.is_match(l.as_slice()) {
 				self.lines.push(l);
 				match command {
-					ListAll => {
+					POP3Command::ListAll => {
 						self.complete = true;
 						self.parse_list_all();
 					},
-					Retr => {
+					POP3Command::Retr => {
 						self.complete = true;
 						self.parse_message();
 					},
@@ -375,7 +375,7 @@ impl POP3Response {
 		let caps = stat_regex.captures(self.lines[0].as_slice()).unwrap();
 		let num_emails = from_str(caps.at(1));
 		let total_email_size = from_str(caps.at(2));
-		self.result = Some(POP3Stat {
+		self.result = Some(POP3Result::POP3Stat {
 			num_email: num_emails.unwrap(),
 			mailbox_size: total_email_size.unwrap()
 		})
@@ -391,7 +391,7 @@ impl POP3Response {
 			let message_size = from_str(caps.at(2));
 			metadata.push(POP3EmailMetadata{ message_id: message_id.unwrap(), message_size: message_size.unwrap()});
 		}
-		self.result = Some(POP3List {
+		self.result = Some(POP3Result::POP3List {
 			emails_metadata: metadata
 		});
 	}
@@ -401,7 +401,7 @@ impl POP3Response {
 		let caps = stat_regex.captures(self.lines[0].as_slice()).unwrap();
 		let message_id = from_str(caps.at(1));
 		let message_size = from_str(caps.at(2));
-		self.result = Some(POP3List {
+		self.result = Some(POP3Result::POP3List {
 			emails_metadata: vec![POP3EmailMetadata{ message_id: message_id.unwrap(), message_size: message_size.unwrap()}]
 		});
 	}
@@ -411,7 +411,7 @@ impl POP3Response {
 		for i in range(1, self.lines.len()-1) {
 			raw.push(String::from_str(self.lines[i].as_slice()));
 		}
-		self.result = Some(POP3Message{
+		self.result = Some(POP3Result::POP3Message{
 			raw: raw
 		});
 	}
